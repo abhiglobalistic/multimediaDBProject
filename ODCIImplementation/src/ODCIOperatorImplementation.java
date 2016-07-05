8f0d5520198590707da727e42656eef9cf529cb0
hbenarab <ben.arab.heni@gmail.com>
2016-07-05 15:16:29 +0200

    adding operation implementation

---
+++ ODCIImplementation/src/ODCIOperatorImplementation.java
@@ -0,0 +1,10 @@
+/**
+ * Created by heni on 7/5/16.
+ */
+public class ODCIOperatorImplementation {
+
+    public static int ImageSimilarity (String pathQImage, String pathDBImage){
+
+        return 1;
+    }
+}
